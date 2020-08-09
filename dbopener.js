7166906e7b60ddcf7a45134c139ce9bdb4701cbc
Shane Goonasekera <46552619+shanerrr@users.noreply.github.com>
2020-08-09 17:25:27 -0600

    Delete dbopener.js

--- dbopener.js
+++
@@ -1,9 +0,0 @@
-var flatfile = require('flat-file-db');
-var db = flatfile('Rdb.db');
- 
-db.on('open', function() {
-    //db.put('hello', "sjdksajdhsaldksakldjkasljdaskldjaksld");  // store some data
-    //db.put('something is wrong hahahahahahahahahahahahahahahahahahahahah', "asdsjdksajdhsaldksakldjkasljdaskldjaksld");  // store some data
-    console.log(db.keys());
-    db.close();
-});
