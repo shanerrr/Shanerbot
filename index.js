6a7f4d57eb82055e49041d59cc27c0e974de5fb3
Shane Goonasekera <theshanukg@gmail.com>
2023-06-02 18:48:44 -0600

    fixing stuff

--- index.js
+++ index.js
@@ -4,72 +4,77 @@ const { token } = require("./config.json");
 const fs = require("node:fs");
 const path = require("node:path");
 
-// Create a new client instance
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
-});
+const main = async () => {
+  // Create a new client instance
+  const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
+  });
 
-// this is the entrypoint for discord-player based application
-client.player = new Player(client, {
-  ytdlOptions: {
-    filter: "audioonly",
-    quality: "highestaudio",
-    highWaterMark: 1 << 25,
-  },
-});
+  // this is the entrypoint for discord-player based application
+  client.player = new Player(client, {
+    ytdlOptions: {
+      filter: "audioonly",
+      quality: "highestaudio",
+      highWaterMark: 1 << 25,
+    },
+  });
+  await client.player.extractors.loadDefault();
 
-// command handler
-client.commands = new Collection();
+  // command handler
+  client.commands = new Collection();
 
-const commandsPath = path.join(__dirname, "commands");
-const commandFiles = fs
-  .readdirSync(commandsPath)
-  .filter((file) => file.endsWith(".js"));
+  const commandsPath = path.join(__dirname, "commands");
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
 
-for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
-  // Set a new item in the Collection with the key as the command name and the value as the exported module
-  if ("data" in command && "execute" in command) {
-    client.commands.set(command.data.name, command);
-  } else {
-    console.log(
-      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-    );
-  }
-}
-
-//listeners
-client.on(Events.InteractionCreate, async (interaction) => {
-  const command = interaction.client.commands.get(interaction.commandName);
-
-  try {
-    if (interaction.isAutocomplete()) {
-      //Handling autocomplete requests
-      await command.autocompleteExecute(client, interaction);
-    } else if (interaction.isChatInputCommand()) {
-      //Handling chat slashCommand requests
-      await command.execute(client, interaction);
-    }
-  } catch (error) {
-    console.error(error);
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
     } else {
-      await interaction.reply({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
     }
   }
-});
 
-client.once(Events.ClientReady, (c) => {
-  console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+  //listeners
+  client.on(Events.InteractionCreate, async (interaction) => {
+    const command = interaction.client.commands.get(interaction.commandName);
 
-// Log in to Discord with token
-client.login(token);
+    try {
+      if (interaction.isAutocomplete()) {
+        //Handling autocomplete requests
+        await command.autocompleteExecute(client, interaction);
+      } else if (interaction.isChatInputCommand()) {
+        //Handling chat slashCommand requests
+        await command.execute(client, interaction);
+      }
+    } catch (error) {
+      console.error(error);
+      if (interaction.replied || interaction.deferred) {
+        await interaction.followUp({
+          content: "There was an error while executing this command!",
+          ephemeral: true,
+        });
+      } else {
+        await interaction.reply({
+          content: "There was an error while executing this command!",
+          ephemeral: true,
+        });
+      }
+    }
+  });
+
+  client.once(Events.ClientReady, (c) => {
+    console.log(`Ready! Logged in as ${c.user.tag}`);
+  });
+
+  // Log in to Discord with token
+  client.login(token);
+};
+
+main();
